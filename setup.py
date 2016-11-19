1c34750ecbe85bc9f8565323727d0958fbdad15d
Tanitter <punigumi@gmail.com>
2016-11-19 22:40:24 +0900

    Try to cythonize when Cython is installed (#8)
    * Try to cythonize when Cython is installed
    
    * Fix wrong version number

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
-from setuptools import setup, find_packages, Extension
 import os.path
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 import warnings
+
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
 
 classifiers = [
     'Programming Language :: Python :: 2',
@@ -10,13 +17,28 @@ classifiers = [
     'Topic :: Scientific/Engineering'
 ]
 
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+ext = '.pyx' if USE_CYTHON else '.cpp'
+
 extensions = [Extension(
         'fastdtw._fastdtw',
-        [os.path.join('fastdtw', "_fastdtw.pyx")],
+        [os.path.join('fastdtw', '_fastdtw' + ext)],
         language="c++",
         include_dirs=[],
         libraries=["stdc++"]
     )]
+
+if USE_CYTHON:
+    extensions = cythonize(extensions)
 
 kwargs = {
     'name': 'fastdtw',
