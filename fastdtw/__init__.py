0b0d6125036b11f00c4fdbf074c770e752422d81
Laurent Vermue <lvermue@users.noreply.github.com>
2019-04-05 18:34:27 +0200

    Update __init__.py file and Cython _fastdtw.cpp file
    The files have been updated to incorporate the respective changes within
    the _fastdtw.pyx and fastdtw.py files.
    	modified:   fastdtw/__init__.py
    	modified:   fastdtw/_fastdtw.cpp

--- fastdtw/__init__.py
+++ fastdtw/__init__.py
@@ -1,4 +1,4 @@
 try:
-    from ._fastdtw import fastdtw, dtw, fastdtw_parallel, get_path
+    from ._fastdtw import fastdtw, dtw, dtw_parallel, fastdtw_parallel, get_path
 except ImportError:
-    from .fastdtw import fastdtw, dtw, fastdtw_parallel, get_path
+    from .fastdtw import fastdtw, dtw, dtw_parallel, fastdtw_parallel, get_path
