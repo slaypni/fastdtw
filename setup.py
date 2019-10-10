530c55d868b9da817f1e354ca1c729fb1ff5ca40
Guillem Duran <guillem@sourced.tech>
2019-10-10 17:48:29 +0200

    Add pyproject.toml so cython and numpy are always pre-installed
    Signed-off-by: Guillem Duran <guillem@sourced.tech>
    
    Update fork
    
    Signed-off-by: Guillem Duran <guillem@sourced.tech>

--- setup.py
+++ setup.py
@@ -1,86 +1,46 @@
 import os.path
-from setuptools import setup, find_packages, Extension
-from setuptools.command.build_ext import build_ext as _build_ext
 import sys
-import warnings
 
-try:
-    from Cython.Build import cythonize
-    USE_CYTHON = True
-except ImportError:
-    USE_CYTHON = False
-
-# numpy path is needed for building with and without cython:
-try:
-    import numpy
-    numpy_includes = [numpy.get_include()]
-    HAVE_NUMPY = True
-except ImportError:
-    # "python setup.py build" will not work and trigger fallback to pure python later on,
-    # but "python setup.py clean" will be successful with the first call of setup(...)
-    numpy_includes = []
-    HAVE_NUMPY = False
+from Cython.Build import cythonize
+import numpy
+from setuptools import Extension, find_packages, setup
 
 classifiers = [
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 3',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: MIT License',
-    'Topic :: Scientific/Engineering'
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Topic :: Scientific/Engineering",
 ]
 
+extensions = cythonize([Extension("fastdtw._fastdtw",
+                                  [os.path.join("fastdtw", "_fastdtw.pyx")],
+                                  language="c++",
+                                  include_dirs=[numpy.get_include()],
+                                  libraries=["stdc++"],)])
 
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-
-
-ext = '.pyx' if USE_CYTHON else '.cpp'
-
-extensions = [Extension(
-        'fastdtw._fastdtw',
-        [os.path.join('fastdtw', '_fastdtw' + ext)],
-        language="c++",
-        include_dirs=numpy_includes,
-        libraries=["stdc++"]
-    )]
-
-if USE_CYTHON:
-    extensions = cythonize(extensions)
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(dir_path, 'README.rst')) as f:
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "README.rst")) as f:
     long_description = f.read()
 
-needs_pytest = set(['pytest', 'test', 'ptr']).intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
+needs_pytest = set(["pytest", "test", "ptr"]).intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 kwargs = {
-    'name': 'fastdtw',
-    'version': '0.3.4',
-    'author': 'Kazuaki Tanida',
-    'url': 'https://github.com/slaypni/fastdtw',
-    'description': 'Dynamic Time Warping (DTW) algorithm with an O(N) time and memory complexity.',
-    'long_description': long_description,
-    'license': 'MIT',
-    'keywords': ['dtw'],
-    'install_requires': ['numpy'],
-    'packages': find_packages(),
-    'ext_modules':  extensions,
-    'test_suite': 'tests',
-    'setup_requires': pytest_runner,
-    'tests_require': ['pytest'],
-    'classifiers': classifiers
+    "name": "fastdtw",
+    "version": "0.3.4",
+    "author": "Kazuaki Tanida",
+    "url": "https://github.com/slaypni/fastdtw",
+    "description": "Dynamic Time Warping (DTW) algorithm with an O(N) time and memory complexity.",
+    "long_description": long_description,
+    "license": "MIT",
+    "keywords": ["dtw"],
+    "install_requires": ["numpy"],
+    "packages": find_packages(),
+    "ext_modules": extensions,
+    "test_suite": "tests",
+    "setup_requires": pytest_runner,
+    "tests_require": ["pytest"],
+    "classifiers": classifiers,
 }
 
-try:
-    setup(**kwargs)
-except SystemExit:
-    del kwargs['ext_modules']
-    reason = 'numpy missing, ' if not HAVE_NUMPY else ''
-    warnings.warn(reason+'compilation failed. Installing pure python package')
-    setup(**kwargs)
+setup(**kwargs)
